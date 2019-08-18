fc7a7d499b42d7304c13a22ddca1be9173b38c50
Peter <peter_raes@telenet.be>
2019-08-18 11:54:23 +0200

    dff

--- testgit2/Form1.cs
+++ testgit2/Form1.cs
@@ -16,5 +16,11 @@ namespace testgit2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("dfdf");
+
+        }
     }
 }
